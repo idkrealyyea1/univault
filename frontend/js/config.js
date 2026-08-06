d567c2ebac53a4d50f32e8d3f132100169f8ecf3
idkrealyyea1 <idkrealyyea1@users.noreply.github.com>
2026-08-06 15:05:16 +0300

    Fix API URL

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -7,6 +7,6 @@
 const UNIVAULT_CONFIG = {
   SUPABASE_URL: 'https://wipxygagzpykdohtxsfk.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpcHh5Z2FnenB5a2RvaHR4c2ZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwMTAzNzgsImV4cCI6MjEwMTU4NjM3OH0.jt8D7nhHzv586XaRCoHLCztmnsmjrlSjb4FZK1LlZDk',
-  API_URL: 'https://univault-api.onrender.com',
+  API_URL: 'https://univault-backend-lss1.onrender.com',
   VAPID_PUBLIC_KEY: 'BEd2CqPEoMWGaOGdE1U6Dcu7DCBFb-1GDEJlfiLdRB_qBtppRKwYRPNBRrQzj5KZJrvZehH8KOgOeX362hw1UMg'
 };
